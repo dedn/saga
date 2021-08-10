7bbdcf6471129e73de7e31422021d6e3ab405b4d
Mac <yurii.kyrylov@litslink.com>
2021-08-10 16:17:02 +0300

    take take every

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,7 +1,20 @@
+import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+
 function App() {
+    const store = useSelector(store => store)
+    const dispatch = useDispatch()
+
+
+    console.log('s', store)
+
   return (
     <h1>
       Redux-saga tutorial: lesson #3
+        <button
+        onClick={()=> dispatch({type:'CLICK'})}>
+          click
+        </button>
     </h1>
   );
 }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,3 +1,21 @@
-export default function* rootSaga() {
+import { takeEvery } from 'redux-saga/effects'
+// import { takeEvery } from "@redux-saga/core/effects";
 
+
+export function* workerSaga() {
+    console.log(' click from saga')
+}
+
+export function* watchClickSaga() {
+    // while (true) {
+    //     yield take('CLICK')
+    //     yield workerSaga()
+    // }
+
+    yield takeEvery('CLICK', workerSaga)
+    // yield workerSaga()
+}
+
+export default function* rootSaga() {
+    yield watchClickSaga()
 }
