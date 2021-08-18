732ad70383749e2e0aef706c399fdf5552a3338d
Mac <yurii.kyrylov@litslink.com>
2021-08-18 10:05:17 +0300

    added retry saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -4,5 +4,18 @@ import peopleSaga from './people';
 export default function* rootSaga() {
     const sagas = [peopleSaga];
 
-    yield all(sagas.map(s => spawn(s)));
+    const retrySagas = yield  sagas.map(saga => {
+        return spawn(function* () {
+            while (true) {
+                try {
+                    yield call(saga)
+                    break
+                } catch (e) {
+                    console.log(e)
+                }
+            }
+        })
+    })
+
+    yield all(retrySagas)
 }
